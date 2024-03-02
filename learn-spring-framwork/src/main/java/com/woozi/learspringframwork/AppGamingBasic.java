1fcc5ae5db8952172be8497bcb57d5283f532dd9
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-03-02 19:47:30 +0900

    강한 결합2

--- learn-spring-framwork/src/main/java/com/woozi/learspringframwork/AppGamingBasic.java
+++ learn-spring-framwork/src/main/java/com/woozi/learspringframwork/AppGamingBasic.java
@@ -2,13 +2,15 @@ package com.woozi.learspringframwork;
 
 import com.woozi.learspringframwork.game.GameRunner;
 import com.woozi.learspringframwork.game.MarioGame;
+import com.woozi.learspringframwork.game.SuperGame;
 
 public class AppGamingBasic {
 
 	public static void main(String[] args) {
 		
+		SuperGame superGame = new SuperGame();
 		MarioGame marioGmae = new MarioGame();
-		GameRunner gameRunner = new GameRunner(marioGmae);
+		GameRunner gameRunner = new GameRunner(superGame);
 		gameRunner.run();
 		
 	}
--- learn-spring-framwork/src/main/java/com/woozi/learspringframwork/game/GameRunner.java
+++ learn-spring-framwork/src/main/java/com/woozi/learspringframwork/game/GameRunner.java
@@ -2,10 +2,15 @@ package com.woozi.learspringframwork.game;
 
 public class GameRunner {
 	
-	MarioGame game;
+//	MarioGame game;
+	SuperGame game;
+	
+//	public GameRunner(MarioGame game) {
+//		this.game = game;
+//	}
 
-	public GameRunner(MarioGame game) {
-		this.game = game;
+	public GameRunner(SuperGame superGame) {
+		this.game = superGame;
 	}
 
 	public void run() {
